f2787593be649fc6478c25b098d662523389105f
Jakub Jeż <kubajez02@gmail.com>
2025-08-07 02:29:41 +0200

    Dostosowywanie modelu

--- training/model.py
+++ training/model.py
@@ -1,15 +1,16 @@
 import torch.nn as nn
 
-
 class Model(nn.Module):
     def __init__(self, num_of_indexes):
         super().__init__()
         self.conv1 = nn.Conv2d(13, 64, kernel_size=3, padding=1)
         self.conv2 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, padding=1)
         self.flatten = nn.Flatten()
         self.batch_norm1 = nn.BatchNorm2d(64)
         self.batch_norm2 = nn.BatchNorm2d(128)
-        self.fc1 = nn.Linear(8 * 8 * 128, 256)
+        self.batch_norm3 = nn.BatchNorm2d(256)
+        self.fc1 = nn.Linear(8 * 8 * 256, 256)
         self.fc2 = nn.Linear(256, num_of_indexes)
         self.relu = nn.ReLU()
 
@@ -23,6 +24,8 @@ class Model(nn.Module):
         x = self.batch_norm1(x)
         x = self.relu(self.conv2(x))
         x = self.batch_norm2(x)
+        x = self.relu(self.conv3(x))
+        x = self.batch_norm3(x)
         x = self.flatten(x)
         x = self.relu(self.fc1(x))
         x = self.fc2(x)
