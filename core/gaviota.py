1dd29abf53303b4513260c9d1c1edbb417998605
Jakub <kubajez02@gmail.com>
2025-10-08 18:03:23 +0200

    Naprawa priorytetu gaviota win->remis->lose

--- core/gaviota.py
+++ core/gaviota.py
@@ -2,13 +2,16 @@ import chess
 import chess.gaviota
 
 def get_move_from_table(board, tb):
+    """Zwraca najlepszy ruch z tabeli końcówek Gaviota dla danej pozycji."""
     best_move = None
+    best_wdl = None  
+    best_dtm = None
+    move_info = []
 
     for mv in board.legal_moves:
         board.push(mv)
-
         try:
-            wdl = tb.probe_wdl(board) 
+            wdl_post = tb.probe_wdl(board)
         except chess.gaviota.MissingTableError:
             board.pop()
             continue
@@ -17,25 +20,46 @@ def get_move_from_table(board, tb):
             continue
 
         try:
-            dtm = tb.probe_dtm(board)
+            dtm_post = tb.probe_dtm(board)
         except chess.gaviota.MissingTableError:
-            dtm = None
+            dtm_post = None
         except Exception:
-            dtm = None
+            dtm_post = None
 
-        if best_move is None and wdl != 0:
-            best_move = (mv, wdl, dtm)
-        else:
-            # Wygrywający
-            if wdl < 0:                    
-                if dtm is not None and dtm > best_move[2]:
-                    best_move = (mv, wdl, dtm)
-            # Przegrywający
-            elif wdl > 0:
-                if dtm is not None and dtm > best_move[2]:
-                    best_move = (mv, wdl, dtm)
+        wdl = -wdl_post
+        dtm = -dtm_post if dtm_post is not None else None
+
+        move_info.append((mv, wdl, dtm))
+
+        if best_move is None:
+            best_move = mv
+            best_wdl = wdl
+            best_dtm = dtm
+            board.pop()
+            continue
+
+        if wdl == 1:
+            if best_wdl != 1:
+                best_move, best_wdl, best_dtm = mv, wdl, dtm
+            else:
+                if dtm is not None and best_dtm is not None:
+                    if abs(dtm) < abs(best_dtm):
+                        best_move, best_wdl, best_dtm = mv, wdl, dtm
+
+        elif wdl == 0:
+            if best_wdl == -1:
+                best_move, best_wdl, best_dtm = mv, wdl, dtm
+
+        elif wdl == -1:
+            if best_wdl == -1:
+                if dtm is not None and best_dtm is not None:
+                    if abs(dtm) > abs(best_dtm):
+                        best_move, best_wdl, best_dtm = mv, wdl, dtm
 
         board.pop()
 
-    return chess.Move.from_uci(best_move[0].uci())
+    if best_move is None:
+        return move_info, None
+
+    return move_info, best_move
 
